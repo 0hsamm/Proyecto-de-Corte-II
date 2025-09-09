0778281c306a18bc4934b956884a42a0ea856847
geova <geova@Andres>
2025-09-08 21:35:14 -0500

    Creacion de los paquetes

--- ProyectoCorteII/src/co/edu/unbosque/controller/Controller.java
+++ ProyectoCorteII/src/co/edu/unbosque/controller/Controller.java
@@ -1,9 +1,7 @@
 package co.edu.unbosque.controller;
 
 public class Controller {
-	private String a;
-	private String hola;
-	
+
 	public Controller() {
 		// TODO Auto-generated constructor stub
 	}
