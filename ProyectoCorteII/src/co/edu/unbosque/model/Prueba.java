c13c0239d06a9a176387db60a64dd182e2b915b1
geova <geova@Andres>
2025-09-08 20:48:09 -0500

    primero

--- ProyectoCorteII/src/co/edu/unbosque/model/Prueba.java
+++ ProyectoCorteII/src/co/edu/unbosque/model/Prueba.java
@@ -3,9 +3,11 @@ package co.edu.unbosque.model;
 public class Prueba {
 	private int opcion;
 	private String texto;
+	private int año;
 	public Prueba() {
 		opcion = 0;
 		texto = "";
+		año = 20;
 	}
-
+	
 }
