199e70f6f19b348f851f234f6fc9ac68aa05b572
Sayis <Sayis@DESKTOP-QG9HKSB>
2025-09-11 23:20:08 -0500

    VentanaMenu

---
+++ ProyectoCorteII/src/co/edu/unbosque/view/VentanaMenu.java
@@ -0,0 +1,65 @@
+package co.edu.unbosque.view;
+
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.Image;
+
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+
+public class VentanaMenu extends JFrame {
+	
+	private JLabel fondoMenu;
+	private JButton salir;
+	
+	public VentanaMenu() {
+		inicializarComponentes();
+	}
+	
+	public void inicializarComponentes() {
+		
+		this.setBounds(10, 10, 1280, 720);
+		this.setTitle("Menu Principal");
+		this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+		this.setLayout(null);
+		this.setResizable(false);
+		this.setLocationRelativeTo(null);
+		
+		ImageIcon fondo = new ImageIcon("src/co/edu/unbosque/view/FondoMenu.PNG");
+		Image escalar = fondo.getImage().getScaledInstance(1280, 720, Image.SCALE_SMOOTH);
+		fondoMenu = new JLabel(new ImageIcon(escalar));
+		fondoMenu.setBounds(0, 0, 1280, 720);
+		this.add(fondoMenu);
+		
+		salir = new JButton("Salir");
+		salir.setFont(new Font("Nunito", Font.BOLD, 40));
+		salir.setBounds(100, 600, 150, 50);
+		salir.setBackground(new Color(254, 230, 1));
+		salir.setForeground(Color.WHITE);
+		salir.setFocusPainted(false);
+		salir.setBorderPainted(false);
+		fondoMenu.add(salir);
+	}
+
+	public JLabel getFondoMenu() {
+		return fondoMenu;
+	}
+
+	public void setFondoMenu(JLabel fondoMenu) {
+		this.fondoMenu = fondoMenu;
+	}
+
+	public JButton getSalir() {
+		return salir;
+	}
+
+	public void setSalir(JButton salir) {
+		this.salir = salir;
+	}
+	
+	
+	
+
+}
