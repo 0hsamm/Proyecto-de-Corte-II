64e47f53caaea02edf9ba33c595868f593aedab3
Sayis <Sayis@DESKTOP-QG9HKSB>
2025-09-11 23:19:39 -0500

    Ventana Menu

--- ProyectoCorteII/src/co/edu/unbosque/controller/Controller.java
+++ ProyectoCorteII/src/co/edu/unbosque/controller/Controller.java
@@ -1,19 +1,42 @@
 package co.edu.unbosque.controller;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import co.edu.unbosque.view.VentanaMenu;
 import co.edu.unbosque.view.ViewFacade;
 
-public class Controller {
+public class Controller implements ActionListener {
 	private ViewFacade vf;
-	
-	
+	private VentanaMenu vm;
+
 	public Controller() {
-		// TODO Auto-generated constructor stub
-	}
-	
-	
-	public void runGUI() {
+		vm = new VentanaMenu();
 		vf = new ViewFacade();
-		vf.getVenInicioSesion().setVisible(true);
 	}
-	
+
+	public void runGUI() {
+		// vf.getVenInicioSesion().setVisible(true);
+
+		vm.setVisible(true);
+		
+		asignarListeners();
+	}
+
+	public void asignarListeners() {
+		vm.getSalir().addActionListener(this);
+		vm.getSalir().setActionCommand("SALIR");
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		String comando = e.getActionCommand();
+
+		switch (comando) {
+		case "SALIR":
+			System.exit(0);
+			break;
+		}
+
+	}
 }
