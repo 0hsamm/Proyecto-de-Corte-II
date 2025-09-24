8dc7f905c900fed334e34c5dab00d3c7bc3ddc12
Sayis <Sayis@DESKTOP-QG9HKSB>
2025-09-24 00:53:04 -0500

    java doc paquetes, falta java doc de ventana comprar y controller

--- ProyectoCorteII/src/co/edu/unbosque/model/persistence/package-info.java
+++ ProyectoCorteII/src/co/edu/unbosque/model/persistence/package-info.java
@@ -1,8 +1,8 @@
 /**
- * 
+ * Paquete que contiene las clases relacionadas con la persistencia de datos,
+ * encargadas de manejar el almacenamiento y la recuperación de información
+ * en la aplicación.
+ * Este paquete permite que los modelos creen, actualicen, eliminen y muestren
+ * sus datos almacenados.
  */
-
 package co.edu.unbosque.model.persistence;
-
-
-
